0bdd8576678ec81cf5e09ef547111fbe2f76723a
unknown <wumi.olabisi@gmail.com>
2024-09-10 11:18:18 +0200

    Modification de contenus : ajout du titre RNCP

--- src/_sections/About.js
+++ src/_sections/About.js
@@ -9,11 +9,17 @@ const About = () => {
         <>
             <div className="flex flex-col-reverse items-center">
                 <Reveal>
+                    <div className="text-center">
+                        <p className="text-lg font-light">Niveau <span className="font-bold">INGÉNIEURE</span> - BAC+5 <a href="https://www.francecompetences.fr/recherche/rncp/35078/" target="_blank" title="Accéder à la fiche de mon titre RNCP">Certification RNCP 35078 de Niveau 7</a></p>
+                    </div>
+                </Reveal>
+                <Reveal>
                     <div className="flex flex-col-reverse items-center mt-10">
                         <h1 className="text-3xl text-white text-center">Développeuse Web Full stack | Paris et région</h1>
                         <p className="uppercase text-4xl font-bold mb-2 text-center">Omowumi OLABISI</p>
                     </div>
                 </Reveal>
+
                 <Popdown delay={0}>
                     <Image src={profilPic} className="object-cover rounded-full shadow-lg mx-none"
                         width="250"
@@ -23,14 +29,16 @@ const About = () => {
             </div>
             <div className="text-center">
                 <Reveal>
-                    <p className="text-xl text-green-300 mb-4">NextJS, ReactJS, ThreeJS, Framer Motion, WordPress, Javascript, SASS/LESS, SEO/SEA, RGAA, UI/UX</p>
+                    <p className="text-xl text-green-300 mb-4">NextJS, ReactJS, Framer Motion, WordPress, Javascript, SASS/LESS, SEO/SEA, RGAA, UI/UX</p>
                 </Reveal>
+
             </div>
             <div className="text-center  md:w-3/4 md:mx-auto">
                 <Reveal>
                     <p className="text-xl font-light">Après plus de 30 sites vitrines et e-commerces réalisés et 6
-                        ans d&apos;expériences dont 4 spécialisés dans le développement web, je suis à la recherche d&apos;un nouveau
+                        ans d&apos;expériences dans le développement web, je suis à la recherche d&apos;un nouveau
                         challenge pour développer des solutions web performantes.</p>
+
                 </Reveal>
             </div>
             <div className="text-center mt-10 mb-24 md:w-3/4 md:mx-auto">
@@ -50,7 +58,7 @@ const About = () => {
                     <div className="md:w-3/5 font-thin">
                         <Reveal>
                             <p className="mt-4"> Je suis une développeuse web polyvalente et entreprenante,
-                                forte d&apos;une expérience de 4 ans en full stack. Ma créativité et ma capacité à apprendre seront des atouts pour répondre à vos challenges techniques.</p>
+                                forte d&apos;une expérience de 6 ans en full stack. Ma créativité et ma capacité à apprendre seront des atouts pour répondre à vos challenges techniques.</p>
                             <p className="mt-4">Je souhaite intégrer une entreprise qui m&apos;offrira des challenges techniques et qui me permettra d&apos;évoluer en tant que développeuse web Full Stack. </p>
                             <p className="mt-4">Dans mes projets personnels, je m&apos;amuse avec la modélisation 3D, l&apos;animation et le montage vidéo,
                                 et je compose des musiques assistées par ordinateur. Ces projets personnels me permettent d&apos;illustrer ma capacité à faire preuve d&apos;initiative et de créativité dans les projets professionnels qui me sont confiés.</p>
